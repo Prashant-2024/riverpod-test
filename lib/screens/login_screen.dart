45dcbe4009abebb8809d1041f60918094446aac4
Prashant Solanki <prashantsolanki2025@gmail.com>
2024-06-16 11:47:58 +0530

    intialised provider for firebase auth

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,27 +1,32 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_auth/components/rounded_button.dart';
 import 'package:riverpod_auth/constants.dart';
 import 'package:riverpod_auth/screens/home_screen.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class LoginScreen extends ConsumerWidget {
+  LoginScreen({super.key});
 
   static const String id = 'login';
 
+//
+//   @override
+//   State<LoginScreen> createState() => _LoginScreenState();
+// }
+//
+// class _LoginScreenState extends State<LoginScreen> {
+//   late String email;
+//   late String password;
+//
+//   final _auth = FirebaseAuth.instance;
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  late String email;
-  late String password;
-
-  final _auth = FirebaseAuth.instance;
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.yellowAccent,
@@ -46,9 +51,10 @@ class _LoginScreenState extends State<LoginScreen> {
                 textAlign: TextAlign.center,
                 keyboardType: TextInputType.emailAddress,
                 decoration: kTextFieldDecoration.copyWith(hintText: "E-Mail"),
-                onChanged: (value) {
-                  email = value;
-                },
+                controller: _emailController,
+                // onChanged: (value) {
+                //   email = value;
+                // },
               ),
               SizedBox(
                 height: 20.0,
@@ -57,9 +63,10 @@ class _LoginScreenState extends State<LoginScreen> {
                 textAlign: TextAlign.center,
                 obscureText: true,
                 decoration: kTextFieldDecoration.copyWith(hintText: "Password"),
-                onChanged: (value) {
-                  password = value;
-                },
+                controller: _passwordController,
+                // onChanged: (value) {
+                //   password = value;
+                // },
               ),
               SizedBox(
                 height: 20.0,
@@ -69,7 +76,8 @@ class _LoginScreenState extends State<LoginScreen> {
                 onPressed: () async {
                   try {
                     final existingUser = await _auth.signInWithEmailAndPassword(
-                        email: email, password: password);
+                        email: _emailController.text,
+                        password: _emailController.text);
                     if (existingUser != null) {
                       Navigator.pushNamed(context, HomeScreen.id);
                     }
